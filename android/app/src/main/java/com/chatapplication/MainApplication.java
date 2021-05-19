877ed7f6fbcc7d53b652071d3fe4bb1fc486b3b1
Sushant Giri <sushantgiri38@gmail.com>
2021-05-19 16:29:03 +0545

    React Navigation and Firebase Set Uo

--- android/app/src/main/java/com/chatapplication/MainApplication.java
+++ android/app/src/main/java/com/chatapplication/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -24,8 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+            // Packages that cannot be autolinked yet can be added manually here, for example:
+          // packages.add(new RNGoogleSigninPackage());
           return packages;
         }
 
---
+++ src/screens/MainScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text
+} from 'react-native';
+
+
+export default class MainScreen extends React.Component {
+
+    render(){
+        return(
+            <View>
+                <Text>Hello World!</Text>
+            </View>
+        )
+    }
+}
