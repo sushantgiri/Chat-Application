0fb90e38bb4a9ae9cdacc850604656fae35412ae
Sushant Giri <sushantgiri38@gmail.com>
2021-05-19 23:04:21 +0545

    Routes and Navigation and  Screens SetUp

--- App.js
+++ App.js
@@ -1,27 +1,9 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import AuthenticationScreen from './src/screens/AuthenticationScreen';
+import React from 'react';
+import { View } from 'react-native';
+import Providers from './navigation';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-const Stack = createStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Chat App" component={AuthenticationScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+const App = () => {
+  return <Providers />;
 }
 
 export default App;
