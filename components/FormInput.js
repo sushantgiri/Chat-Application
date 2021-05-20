cb8b63928d93ae4b3f09edf7edf05923ba3383bf
Sushant Giri <sushantgiri38@gmail.com>
2021-05-20 13:12:42 +0545

    Added Vector Icons, Google Login Functionality

--- components/FormInput.js
+++ components/FormInput.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import {View, TextInput, StyleSheet} from 'react-native';
 import {windowHeight, windowWidth} from '../utils/Dimentions';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
 
 
 const FormInput = ({labelValue, placeholderText, iconType, ...rest}) => {
   return (
     <View style={styles.inputContainer}>
+
+<View style={styles.iconStyle}>
+        <AntDesign name={iconType} size={25} color="#666" />
+      </View>
       
       <TextInput
         value={labelValue}
